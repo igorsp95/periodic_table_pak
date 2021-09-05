4bbec16742cd876879383326d30aceaa9cfc3739
igorparente <igorscheidegger@hotmail.com>
2021-09-05 17:40:59 -0300

    app features done

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,6 +1,11 @@
 class TableController < ApplicationController
   def index
-    file = File.read("db/data.json")
-    @elements = JSON.parse(file)
+    # file = File.read("db/data.json")
+    # @elements = JSON.parse(file)
+    @elements = Element.all
+  end
+
+  def show
+    @element = Element.find(params[:id])
   end
 end
